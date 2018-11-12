77b0d6b33887e47dc506ef0eaa333f4a2d59864c
MicaPatino <mica.patino@hotmail.com>
2018-11-11 23:26:25 -0300

    changes

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const path =require('path')
+var bodyParser = require('body-parser');
 
  
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname + '/public/index.html'));  
@@ -17,13 +19,37 @@ app.get('/login', function(request, response) {
   response.sendFile(path.join(__dirname + '/public/login.html'));  
 });
 
+app.post('/login', function(request,response){
+	var login = JSON.parse(request.body.login);
+	console.log(login,users)
+	var result = users.filter(function (user) {
+		if(user.password == login.password && user.user == login.username){
+		return user;
+	}});
+	console.log(result)
+	if(result.length > 0) {
+		response.json({status:200});
+	}
+	  else {
+	  	response.json({status:404});
+		}		  
+});
+
 app.get('/register', function(request, response) {
   response.sendFile(path.join(__dirname + '/public/register.html'));  
+});
+
+var users = [];
+app.post('/register',function(request, response) {
+	users.push(JSON.parse(request.body.user));
+	response.json({status:200});
+
 });
 
 app.get('/account', function(request, response) {
   response.sendFile(path.join(__dirname + '/public/account.html'));  
 });
+
 
 app.listen(3000, function() {
   console.log('server express ready!');
